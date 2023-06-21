6336af97f612629bdb14c511d3e4c2f5e7652ad2
acercompaq <giggalante@hotmail.it>
2023-06-21 16:10:11 +0200

    first commit

---
+++ aspnet_client/system_web/1_1_4322/_vti_cnf/SmartNav.js
@@ -0,0 +1,7 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|15 Jul 2004 00:40:30 -0000
+vti_extenderversion:SR|5.0.2.2623
+vti_lineageid:SR|{A07F1E75-CE19-4275-A9F3-FC267749D83B}
+vti_cacheddtm:TX|15 Jul 2004 00:40:30 -0000
+vti_filesize:IR|8571
+vti_backlinkinfo:VX|
---
+++ aspnet_client/system_web/1_1_4322/_vti_cnf/WebUIValidation.js
@@ -0,0 +1,7 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|15 Jul 2004 00:40:28 -0000
+vti_extenderversion:SR|5.0.2.2623
+vti_lineageid:SR|{98CCA8E5-262E-467A-92ED-B8525CCF8957}
+vti_cacheddtm:TX|15 Jul 2004 00:40:28 -0000
+vti_filesize:IR|14482
+vti_backlinkinfo:VX|
